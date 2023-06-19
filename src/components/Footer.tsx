11d4dc7a3d3c3639028fe1b2bb4dbdf3e3255d6c
EdamAme <121654029+EdamAme-x@users.noreply.github.com>
2023-06-19 19:00:09 +0900

    footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,8 +1,21 @@
+import Link from "LiberComp/Link.tsx";
 
 export default function Header() {
     return (
-        <footer>
-            testes
+        <footer className="bg-gray-800 text-white h-auto w-screen">
+            <div>
+                <p className="text-white text-center p-2 text-xl">Links</p>
+                <div className="grid grid-cols-2 sm:grid-cols-4 gap-2">
+                    <Link to="/" className="text-center hover:bg-gray-600">Top</Link>
+                    <Link to="#example" className="text-center hover:bg-gray-600">Example</Link>
+                    <Link to="/contact" className="text-center hover:bg-gray-600">Contact</Link>
+                    <Link to="/" className="text-center hover:bg-gray-600">GitHub</Link>
+                </div>
+            </div>
+
+            <div className="text-center p-2">
+                &copy; 2023 amex / <a href="https://twitter.com/macl2189" target="_blank">macl2189</a> All rights reserved.  
+            </div>
         </footer>
     )
 }
