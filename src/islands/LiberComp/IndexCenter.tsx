874b8845dde4484b74dcdcb07ff08e958c0eac7b
EdamAme <121654029+EdamAme-x@users.noreply.github.com>
2023-06-20 10:55:23 +0900

    ch

--- src/islands/LiberComp/IndexCenter.tsx
+++ src/islands/LiberComp/IndexCenter.tsx
@@ -14,16 +14,16 @@ function IndexCenter() {
     return (
       <>
         <div style={{ filter: "blur(0px)" }}>
-          <h2 className="text-2xl font-extrabold text-4xl sm:text-5xl lg:text-6xl tracking-tight text-center dark:text-white w-screen absolute top-[80px] left-1/2 -translate-x-1/2 -translate-y-1/2">
+          <h2 style={{ filter: "blur(0px)" }} className="text-2xl font-extrabold text-4xl sm:text-5xl lg:text-6xl tracking-tight text-center dark:text-white w-screen absolute top-[80px] left-1/2 -translate-x-1/2 -translate-y-1/2">
             <span className="text-gray-300">Welcome</span> to{" "}
             <span className="text-green-500">Liber</span>{" "}
             <span className="text-red-500">AD</span>{" "}
             <span className="text-blue-500">!</span>
           </h2>
 
-          <h3 className="font-extrabold text-xl sm:text-2xl lg:text-3xl tracking-tight text-center dark:text-white w-screen absolute top-[150px] left-1/2 -translate-x-1/2 -translate-y-1/2">
-            LiberAD は、無料の広告ネットワークサービスです。<br/>
-            自由にサイトに広告を設置できます。条件は学生であること。
+          <h3 style={{ filter: "blur(0px)" }} className="font-extrabold text-xl sm:text-xl lg:text-2xl tracking-tight text-center dark:text-white w-screen absolute top-[150px] left-1/2 -translate-x-1/2 -translate-y-1/2">
+            LiberAD は無料の広告ネットワークサービスです。<br/>
+            自由にサイトに広告を設置できます。条件は学生であること
           </h3>
         </div>
       </>
@@ -34,11 +34,11 @@ function IndexCenter() {
     <div className="w-screen">
       <div
         className="w-screen h-[300px] bg-gray-900 overflow-x-hidden select-none"
-        style={{ filter: "blur(1px)" }}
       >
         <div>{getVisual()}</div>
         <div
           className="w-[1000px] break-none"
+          style={{ filter: "blur(1.2px)" }}
           dangerouslySetInnerHTML={{ __html: mainAA }}
         ></div>
       </div>
