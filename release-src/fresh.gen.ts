c39a04f338fb37bb9fb006a3cdfdb2d14e5d0da4
EdamAme <121654029+EdamAme-x@users.noreply.github.com>
2023-06-21 21:35:48 +0900

    fix

--- release-src/fresh.gen.ts
+++ release-src/fresh.gen.ts
@@ -3,10 +3,10 @@
 // This file is automatically updated during development when running `dev.ts`.
 
 import * as $0 from "./routes/_404.tsx";
-import * as $1 from "./routes/api/LiberAD.tsx";
-import * as $2 from "./routes/api/getSites.tsx";
+import * as $1 from "./routes/api/LiberAD.ts";
+import * as $2 from "./routes/api/getSites.ts";
 import * as $3 from "./routes/api/liberad.mjs";
-import * as $4 from "./routes/api/sites.tsx";
+import * as $4 from "./routes/api/sites.ts";
 import * as $5 from "./routes/index.tsx";
 import * as $6 from "./routes/start.tsx";
 import * as $$0 from "./islands/CodeGen.tsx";
